cbd71be9bdeea7ce5144c2b786f7a87b4dfbeeab
Rahul <rahulpratik1212@gmail.com>
2025-02-09 13:12:45 +0530

    add select field for pages

--- pagination/src/components/PaginationButton.tsx
+++ pagination/src/components/PaginationButton.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
-
-const arr = [1, 2, 3, 4, 5];
+import { pageArray } from "../utils/constants";
 
 const PaginationButton = ({ page, setPage, totalLength }) => {
+  const [pages, setPages] = React.useState("");
+
+  console.log(totalLength);
+
+  const handleChange = (event: any) => {
+    console.log(event);
+    setPages(page);
+  };
+
   const handlePrev = () => {
     if (page > 1) setPage(page - 1);
   };
@@ -29,11 +46,11 @@ const PaginationButton = ({ page, setPage, totalLength }) => {
         >
           <KeyboardArrowLeft sx={{ fontSize: 30 }} />
         </Button>
-        {arr.map((data, index) => (
+        {pageArray.map((data, index) => (
           <Button
             onClick={() => setPage(data)}
             key={index}
-            variant="outlined"
+            variant={page === data ? "contained" : "uncontained"}
             sx={{ paddingX: 1.8, paddingY: 1.8, borderRadius: "50%" }}
           >
             <Typography fontSize={20}>{index + 1}</Typography>
@@ -43,7 +60,7 @@ const PaginationButton = ({ page, setPage, totalLength }) => {
           <>
             <Button variant="text">...</Button>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{ paddingX: 1.8, paddingY: 1.8, borderRadius: "50%" }}
             >
               <Typography fontSize={20}>{page}</Typography>
@@ -58,10 +75,35 @@ const PaginationButton = ({ page, setPage, totalLength }) => {
           <KeyboardArrowRight sx={{ fontSize: 30 }} />
         </Button>
       </Stack>
-      <Box>
+      <Box
+      mt={4}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
         <Typography>
           Page {page} of {Math.ceil(totalLength / 10)}
         </Typography>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel id="demo-select-small-label">Pages</InputLabel>
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={pages}
+            label="Pages"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={page}>1</MenuItem>
+            <MenuItem value={page}>2</MenuItem>
+            <MenuItem value={page}>3</MenuItem>
+          </Select>
+        </FormControl>
       </Box>
     </>
   );
